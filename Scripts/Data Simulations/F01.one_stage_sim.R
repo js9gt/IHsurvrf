2a4b22fd529fd7a0bcee37b8ef798a9a0c6bdea7
Jane She <js9gt@unc.edu>
2024-03-12 19:22:38 -0400

    modifying to truncate at tau, factor in censoring for this, and keep count of actions

--- Scripts/Data Simulations/F01.one_stage_sim.R
+++ Scripts/Data Simulations/F01.one_stage_sim.R
@@ -1,20 +1,35 @@
 
 # use this to generate propensity score
-source("C21.sim_params.R")
+#source("C21.sim_params.R")
+source("~/survrf/Scripts/Data Simulations/C21.sim_params.R")
 
 ## Function starts with w/ these @return which shows what the function is supposed to output
 ## This serves as documentation fo the function
 
 #' @return event.time, "visit length"; X = min(Tk, Uk), earlier of time to death or time to next visit
-#' @return failure.time: Tk: failure time
 #' @return gamma 1(T <= U); T = failure time, U = treatment time, which is a failure indicator. 1 means subject fails before next stage.
-#' @return state; based on generated value from a uniform distribution
-#' @return delta: delta is 1 if a patient is not censored, 0 if a patient is censored AKA their cumulative time is greater than tau
-#                        this can also happen if a patient's generated censoring time happens first 
+#' @return delta: delta is 1 if a patient is not censored, 0 if a patient is censored AKA their cumulative time is <= tau
+#                        this can also happen if a patient's generated censoring time happens first
+#' @return failure.time: Tk: failure time
+#' @return treatment.time: Uk: treatment time (if this is min pt moves to next stage)
+#' @return censor.time: Uk: censoring time (if this is min pt is censored at the current stage)
+#' @return time.max: remaining life AKA that the patient has-- this is needed to bound their time up to tau
+                ## if a patient's generated time exceeds time.max, it is truncated at time.max
+#' @return action: binary-- if a patient fails at a stage, they should not receive a treatment?
+#' @return state; based on generated value from a uniform distribution and then updated based on dependency
+#' @return prior.visit.length: length of the previous stage (in terms of days)
+#' @return nstages: number of previous stages (as a proportion of the maximum number of stages)
 
 
-#' @return rate_failure: to track rate (mean) which generates the exponential distribution failure time
-#' @return rate_time_next_visit: to track rate (mean) which generates the exponential distribution
+
+#' @return rate.failure: to track rate (mean) which generates the exponential distribution failure time
+#' @return rate.next.visit: to track rate (mean) which generates the exponential distribution-- time to next visit
+#' @return rate.censoring: to track rate (mean) which generates the exponential distribution-- time to censoring
+
+## output from the multistage_sim section:
+
+#' @return at.risk: 1 if the patient is still at risk, 0 if not (then put all their variables to NA) bc they're ineligible
+#' @return cumulative.time: a proportion of tau (so that our rates don't get too small)= sum of previous visits / tau
 
 
 ## no longer:
@@ -54,61 +69,62 @@ one_stage <- function(
   ## length of of the prior visit (not cumulative)
   prior.visit.length = 0,
   at.risk = 1,
+  time.max = 10000,
   #action = 0, #only used when we first generate prop scores for all stages at once, but this doesn't make sense
-  terminal.stage = FALSE, 
+  terminal.stage = FALSE,
   initial.stage = FALSE,
-  input.state.value = 0, 
+  input.state.value = 0,
   a1 = -0.3, b1 = 0.1, z1 = -0.3, p1 = -1, g1 = -0.2, h1 = 0.2, r1 = -0.8,
   a2 = 1.2, b2 = -0.05, z2 = -2.5, p2 = 0.1, g2 = -2, h2 = 0.6, r2 = -1,
   a3 = 1.2, b3 = -0.05, z3 = -2.5, p3 = 0.1, g3 = -2, h3 = 0.6, r3 = -1,
   tau = 50,
   #dimensions of covariates for state vector generated
   p = 1) {
-  
+
   while (TRUE) {
     if (!at.risk) {
       # If not at risk, assign NA values to output variables
       output = c(event.time = NA, gamma = NA, delta = NA,
-                 failure.time = NA, treatment.time = NA, censor.time = NA, 
-                 action = NA, state = NA, prior.visit.length = prior.visit.length, rate.failure = NA,
+                 failure.time = NA, treatment.time = NA, censor.time = NA, time.max = NA,
+                 action = NA, state = NA, prior.visit.length = prior.visit.length, nstages = NA, rate.failure = NA,
                  rate.next.visit = NA, rate.censoring = NA)
       return(output)
     }
-    
+
     valid_failure_time = FALSE
     while (!valid_failure_time) {
 
-      
+
       # Terminal stage (input) == T; simulation has reached the final phase, no further treatment
       if (initial.stage) {
         # Generate state data from a uniform(0, 1) distribution to control values
         state = runif(1, 0, 1)
-        
+
       } else {
         # if it's not the initial stage, input the value from the updated state (using the formulas)
         state = input.state.value
       }
-      
+
       ## generates predicted probability of treatment for each patient as a vector, inputting generated state
       propensity_scores <- propensity_function(state = state, p)
-      
+
       action <- rbinom(n = 1, size = 1, prob = propensity_scores)
-      
-      
+
+
       # Rate of failure time (Tk)
       rate_failure = exp(
-        a1 + b1 * state + z1 * nstages + p1 * cumulative_length + g1 * action + h1*prior.visit.length + 
+        a1 + b1 * state + z1 * nstages + p1 * cumulative_length + g1 * action + h1*prior.visit.length +
           r1 * action * state * nstages * cumulative_length*prior.visit.length
       )
-      
-      
+
+
       # Rate of time to next visit (Uk)
       rate_time_next_visit = exp(
         a2 + b2 * state + z2 * nstages + p2 * cumulative_length + g2 * action + h2*prior.visit.length +
           r2 * action * state * nstages * cumulative_length*prior.visit.length
       )
-      
-      
+
+
       # Rate of time to censoring (Ck)
       #delta is 1 if a patient is not censored, 0 if a patient is censored
       rate_censoring = exp(
@@ -116,81 +132,88 @@ one_stage <- function(
           r3 * action * state * nstages * cumulative_length*prior.visit.length
       )
 
-      
+
       # Generating failure time (Tk) using the rate from an exponential distribution
-      failure.time = rexp(n = 1, rate = rate_failure) 
-      
+      failure.time = rexp(n = 1, rate = rate_failure)
+
       # Generating time to next visit (treatment.time) (Uk) using the rate from an exponential distribution
-      treatment.time = rexp(n = 1, rate = rate_time_next_visit) 
-      
+      treatment.time = rexp(n = 1, rate = rate_time_next_visit)
+
       # Generating time to censoring (censor.time) (Ck) using the rate from an exponential distribution
-      censor.time = rexp(n = 1, rate = rate_censoring) 
-      
+      censor.time = rexp(n = 1, rate = rate_censoring)
+
       ## delta is 1 if a patient is not censored, 0 if a patient is censored AKA their cumulative time is greater than tau
       ## delta is also 1 if censoring time is smaller than the treatment.time and failure.time
-      
+
       ## cumualative length < 1 since it's a proportion of tau
       delta = (censor.time > min(failure.time, treatment.time) & cumulative_length < 1)
-      
-      
-      
+
+
+
       ### this while loop with the conditions of bounds is what causes the process to take a long time
       if (!is.na(failure.time) && failure.time!= 0 && !is.na(treatment.time) && treatment.time != 0 && !is.na(censor.time)
           && censor.time !=0){
         valid_failure_time = TRUE
-      } 
+      }
     }
-  
-    
-    
+
+
+
     # Terminal stage (input) == T; simulation has reached the final phase, no further treatment
     if (terminal.stage) {
       trt.time = Inf
-      
+
     } else {
       # Terminal stage = F, find the index of the first occurrence where rho <= 1
       trt.time = treatment.time
     }
-    
-    
+
+
     # Summary statistics for event times
     X = min(failure.time, trt.time)
+
+    # Adjust X if greater than time.max
+    X = ifelse(X > time.max, time.max, X)
+
+    ## if we truncate the value of X at time.max, delta would also need to be 0
+    delta = ifelse(X == time.max, 0, delta)
+
     gamma = failure.time <= trt.time
-    
-    
+
+
     # if the patient is censored (delta == 0), then their gamma is UNKNOWN, so it should be NA
     ## we also need to change their event.time to be either be 0, or tau - x (they cannot go beyond tau)
     if (!delta) {
       gamma = NA
-      
-      
+
+
     } else {
       # otherwise, make no change to gamma
       gamma = gamma
     }
-    
+
     # Construct output vector of relevant variables and their values
     output = c(event.time = X, gamma = gamma, delta = delta,
-               failure.time = failure.time, treatment.time = trt.time, censor.time = censor.time,
-               action = action, state = state, prior.visit.length = prior.visit.length, rate.failure = rate_failure,
+               failure.time = failure.time, treatment.time = trt.time, censor.time = censor.time, time.max = time.max,
+               action = action, state = state, prior.visit.length = prior.visit.length, nstages = nstages, rate.failure = rate_failure,
                rate.next.visit = rate_time_next_visit, rate.censoring = rate_censoring)
-    
+
     # Return a list containing statistics
     return(output)
   }
 }
 
 ## vectorizing the following arguments
-one_stage.vec <- Vectorize(one_stage, vectorize.args = c("nstages", "cumulative_length", "input.state.value", "at.risk", "prior.visit.length"))
+one_stage.vec <- Vectorize(one_stage, vectorize.args = c("nstages", "cumulative_length", "input.state.value", "at.risk", "prior.visit.length", "time.max"))
 
 ### example code after fitting propensity model in F02.multistage_sim.R
 #dynamics.vec(nstages = rep(0, 5),
 #         cumulative_length = rep(0, 5),
 #         at.risk = rep(1, 5),
 #         propensity_model = propensity_model,
-#         terminal.stage = FALSE, 
+#         terminal.stage = FALSE,
 #         initial.stage = FALSE,
-#         input.state.value = rep(0, 5), 
+#         input.state.value = rep(0, 5),
 #         a1 = -0.3, b1 = 0.1, z1 = -0.3, p1 = -1, g1 = -0.2, h1 = 0.2, r1 = -0.8,
 #         a2 = 1.2, b2 = -0.05, z2 = -2.5, p2 = 0.1, g2 = -2, h2 = 0.6, r2 = -1,
 #         a3 = 1.2, b3 = -0.05, z3 = -2.5, p3 = 0.1, g3 = -2, h3 = 0.6, r3 = -1,
--- Scripts/Data Simulations/F02.multistage_sim.R
+++ Scripts/Data Simulations/F02.multistage_sim.R
@@ -1,6 +1,8 @@
 
-source("F01.one_stage_sim.R")
-source("F03.Figs.R")
+#source("F01.one_stage_sim.R")
+source("~/survrf/Scripts/Data Simulations/F01.one_stage_sim.R")
+#source("F03.Figs.R")
+source("~/survrf/Scripts/Data Simulations/F03.Figs.R")
 #~/survrf/Scripts/F01.one_stage_sim.R
 #~/survrf/Scripts/F03.Figs.R
 
@@ -21,8 +23,8 @@ simulate_patients <- function(n.sample, max_stages, tau,
                               at.risk = rep(1, n.sample),
                               ## Life so far lived at the beginning of the stage
                               cumulative.time = rep(0, n.sample),
-                              
-                              ## inital length of previous visits 
+
+                              ## inital length of previous visits
                               prior.visit.length = rep(0, n.sample),
                               ## dimensions of the state vector generated at each stage
                               p = 1,
@@ -31,234 +33,256 @@ simulate_patients <- function(n.sample, max_stages, tau,
                               a3 = -4.5, b3 = -1, z3 = -2.5, p3 = -0.1, g3 = 0.2, h3 = -0.6, r3 = -1,
                               ## "covariate" value for later state generation
                               rho = 0.5,
-                              # action-specific effects 
+                              # action-specific effects
                               D0 = 0,
                               D1 = 1,
                               g = 1) {
-  
+
   ## if length of at.risk (input) is 1, then replicate the value until it matches the length of n.sample
   if (length(at.risk) == 1) at.risk = rep(at.risk, n.sample)
-  
+
   ## if length of at.risk (input) is 1, then replicate the value until it matches the length of n.sample
   if (length(cumulative.time) == 1) cumulative.time = rep(cumulative.time, n.sample)
-  
+
   ## if prior visit length is 0, then replicate the value until it matches the length of n.sample
   if (length(prior.visit.length) == 1) prior.visit.length = rep(prior.visit.length, n.sample)
-  
-  require(dplyr)
-  
-  ######################################### TO DELETE AFTER CLARIFICATION
-  ## generating initial baseline state
-  ## each patient has a randomly generated baseline covariate of State (S_0)
-  #baseline_covariates <- simulate_baseline_covariates(n.sample, p)
-  
-  #colnames(baseline_covariates) = c("S_0")
-  
-  
-  ## generates predicted probability of treatment for each patient as a vector
-  #propensity_scores <- propensity_function(state = baseline_covariates, p)
-  
-  ########################################## 
 
-  
+
+  require(dplyr)
+
+
+
   # skeleton
-  ## uses the dynamics.vec() function to create an initial structure to set up an initial state for 
+  ## uses the dynamics.vec() function to create an initial structure to set up an initial state for
   ## later dynamics of the multi-stage
-  tmp <- 
-  
-    one_stage.vec(nstages = 0, cumulative_length = 0, prior.visit.length = 0, at.risk = 1,
+  tmp <-
+
+    one_stage.vec(nstages = 0, cumulative_length = 0, prior.visit.length = 0, at.risk = 1, time.max = 1000,
                   ## using any action for now just to get column names for the structure
-                  terminal.stage = F, initial.stage = T, input.state.value = 0, 
+                  terminal.stage = F, initial.stage = T, input.state.value = 0,
                   a1 = -4.5, b1 = -1, z1 = -0.07, p1 = -0.05, g1 = 0.7, h1 = -0.2, r1 = -0.05,
                   ## 2 for time to next visit
-                  a2 = -3.9, b2 = -1, z2 = -0.008, p2 = -0.01, g2 = -0.7, h2 = -0.2, r2 = -0.005, 
+                  a2 = -3.9, b2 = -1, z2 = -0.008, p2 = -0.01, g2 = -0.7, h2 = -0.2, r2 = -0.005,
                   a3 = -4.5, b3 = -2, z3 = -0.1, p3 = -0.1, g3 = 0.2, h3 = -0.4, r3 = -0.05,
                   tau = tau, p = p) %>% t
-  
-  
-  
+
+
+
   ### test
   #n.sample = 10
-  
+
   # n.stages for HC, max_stages for JS
   #n.stages = 5
-  
+
   #max_stages = 5
   # number of covariates
   #p = 6
-  
+
   ####### COMPARING #####
   # HC generate covariate matrix
   #covariate = mvrnorm(n.sample, rep(0, p), diag(p) + 0.2 - diag(p) * 0.2)
   #colnames(covariate) = paste0("Z", 1:p)
   ###
   ######################
-  
+
   ## initializes an array called "output" filled with NA values with dimensions:
   ##        number of samples being simulated (input)
   ##        dim(tmp)[2]: number of columns in the tmp opject (AKA the output variables from dynamics.vec)
   ##        n.stages: number of stages (input) in simulation
-  output <-  array(NA, dim = c(n.sample, 2 + dim(tmp)[2] + 2, max_stages), 
+  output <-  array(NA, dim = c(n.sample, 2 + dim(tmp)[2] + 2 + 2, max_stages),
                    ## row and column names for the array
-                   dimnames = list(1:n.sample, c("subj.id", "rep.id", colnames(tmp), "at.risk", "cumulative.time"),
-                                                 #colnames(baseline_covariates)), 
+                   dimnames = list(1:n.sample, c("subj.id", "rep.id", colnames(tmp), "at.risk",
+                                                 ## new column to track number of each treatment
+                                                 "action_1_count",
+                                                 "action_0_count",
+                                                 "cumulative.time"),
+                                                 #colnames(baseline_covariates)),
                                    1:max_stages))
-  
-  
+
+
   ## initializes the subj.id column we just created in the output from 1 to n.sample
   output[, "subj.id", ] <- 1:n.sample
-  
+
   ## initialize rep.id column with 0s
   output[, "rep.id", ] <- 0           # rep.id is reserved for later use (repeated random draws)
-  
-  
+
+
+
   ## initialize an input.state vector of 0
   input.state <- rep(0, n.sample)
-  
+
+  ## initialize action counts as 0's
+  action_1_count <- rep(0, n.sample)
+  action_0_count <- rep(0, n.sample)
+
+  ## initialize time.max as a vector with the value being tau at the first stage
+  time.max <- rep(tau,n.sample)
+
   ## iterate through each stage
   for (stage in 1:max_stages) {
-    
+
     ## print the current stage number
     cat("stage ", stage, "\n")
-    
-    
+
+
     ## update the at.risk column in the output array for the current stage using the value from at.risk
     output[, "at.risk", stage]           <- at.risk
-    
-    
+
+
     ## update the cumulative time column in the output array for the current stage using the cumulative vector
     ## to avoid it being overly large, have this be a proportion of tau
     output[, "cumulative.time", stage]     <- cumulative.time
-    
+
+    ## calculates maximum time allowed in simulation based on diff between tau (limit) and existing time lived
+    ## this should be updated at each stage: recall that cumulative.time is a proportion of tau
+    ## initialize this as the input
+    output[, "time.max", stage] = time.max
+
+    ## initialize the counts of actions with 0's
+    output[,"action_1_count", stage] <- action_1_count
+    output[, "action_0_count", stage] <- action_0_count
+
+
     ## updates corresponding matching column names of the baseline covariates with the values in there
     #output[, colnames(baseline_covariates), stage] <- as.matrix(baseline_covariates)
-    
-    
-    
-    stage.output <- 
-      
+
+
+
+    stage.output <-
+
       ## generate values for current stage
       ## for nstages, to avoid the value being overly large, have it also be a proportion of the total number of stages
-      one_stage.vec(nstages = (stage - 1)/max_stages, cumulative_length = cumulative.time, at.risk = at.risk, 
+      one_stage.vec(nstages = (stage - 1)/max_stages, cumulative_length = cumulative.time, at.risk = at.risk,time.max = time.max,
                     prior.visit.length = prior.visit.length,
                     #action = as.numeric(output[, "action", stage]),  ### remove this after clarification
-                    terminal.stage = (stage == max_stages), input.state.value = input.state, 
+                    terminal.stage = (stage == max_stages), input.state.value = input.state,
                     ## if initial stage is true (at stage 1), then use uniform(0, 1), otherwise, use the input
-                    initial.stage = (stage == 1), 
+                    initial.stage = (stage == 1),
                     a1 = a1, b1 = b1, z1 = z1, p1 = p1, g1 = g1, h1 = h1, r1 = r1,
-                    a2 = a2, b2 = b2, z2 = z2, p2 = p2, g2 = g2, h2 = h2, r2 = r2, 
+                    a2 = a2, b2 = b2, z2 = z2, p2 = p2, g2 = g2, h2 = h2, r2 = r2,
                     a3 = a3, b3 = b3, z3 = z3, p3 = p3, g3 = g3, h3 = h3, r3 = r3, tau = tau) %>% t
-    
-    
-      
+
+
+
       ## generate values for current stage
-      #one_stage.vec(nstages = 2, 
+      #one_stage.vec(nstages = 2,
       #              # each patient's cumulative time is 0
-      #              cumulative_length = cumulative.time, 
+      #              cumulative_length = cumulative.time,
       #              # each patient is at.risk at first
       #              at.risk = at.risk,
       #              ## for action vector use the one generated in the "action" column earlier
       #              ## these are different actions FOR EACH PATIENT, so we need to pull the values across each stage
-      #              action = rep(1, n.sample),  
+      #              action = rep(1, n.sample),
       #              terminal.stage = F,
       #              a1 = -7, b1 = 0.1, z1 = -0.3, p1 = -1, g1 = -0.2, r1 = -0.8,
       #              a2 = -0.1, b2 = -0.05, z2 = -2.5, p2 = 0.1, g2 = -2, r2 = -1, tau = tau) %>% t
-    
 
-    
+
     ## combining data from the stage.output with "action" and "at.risk" into a matrix format
     ##      combined data includes info on dynamics of simulation, actions, status of individuals
     output[, c(colnames(tmp),"at.risk"), stage] <- cbind(stage.output, at.risk)
-    
+
     ## whether a patient is at risk is updated based on values of gamma and delta. gamma = 0 means at risk
     # Only those with gamma == 0 is at risk. Those with gamma = NA or 1 are not available.
     ## once delta is 0, the patient is no longer at risk also, only those who are non-censored (delta == 1) are at are risk
-    at.risk <- (stage.output[, "gamma"] == 0 & stage.output[, "delta"] == 1)  
-    
+    at.risk <- (stage.output[, "gamma"] == 0 & stage.output[, "delta"] == 1)
+
     ## replaces NA values in at.risk with 0 (not at risk)
     at.risk[is.na(at.risk)] = 0
-    
+
     ##
-    ### replace not at risk with NA in action 
+    ### replace not at risk with NA in action
     ##
     ##
-    
+
     ## update the cumulative to prepare for next state calculations by adding the current stage's event time
     ## to avoid it being overly large, have this be a proportion of tau
     cumulative.time <- (cumulative.time*tau + stage.output[, "event.time"]) / tau # updating for the next stage
-    
+
+
+    time.max <- tau - (cumulative.time*tau)
+
     ## update the prior visit length column in the output array for the previous stage
     prior.visit.length    <- stage.output[, "event.time"]
-    
+
+    # Update action count vectors for action 1
+    action_1_count <- action_1_count + (stage.output[, "action"] == 1)
+
+    # Update action count vectors for action 0
+    action_0_count <- action_0_count + (stage.output[, "action"] == 0)
+
+
+
+
 
       # if it's not the initial stage, input the value from the updated state (using the formulas)
       # this will use values from the previous state
-      # NOTE: this lags behind by one iteration, that's why we can update the prior.visit.length like we do above 
-    
-        ## we have first D1 term with the I() be opposite effects to find optimal rule immediately 
+      # NOTE: this lags behind by one iteration, that's why we can update the prior.visit.length like we do above
+
+        ## we have first D1 term with the I() be opposite effects to find optimal rule immediately
       input.state = (rho*stage.output[, "state"] + ifelse(stage.output[, "state"] > 0.5, 1, 0)*(D0 - stage.output[, "action"]*D1) +
         ifelse(stage.output[, "state"] < 0.5, 1, 0)*(D0^2 + stage.output[, "action"]*(D1)^2) +
-        0.5*g*(1-(rho)^2)^0.5 * replicate(n.sample, runif(n = 1, min = 0, max = 1))- 
-                                            
-                                            ## subtracting a constant value to get an average 
+        0.5*g*(1-(rho)^2)^0.5 * replicate(n.sample, runif(n = 1, min = 0, max = 1))-
+
+                                            ## subtracting a constant value to get an average
                                             0)
-      
-      
+
+
     ## if the cumulative.time >1, we reset it to 0 and use that to plug in to the next stage
       ## we also need to subtract the difference from the previous stage's event.time
-      
+
       #if (any(!is.na(cumulative.time) & cumulative.time > 1)) {
-        
-        
+
+
       #  cumulative.time[cumulative.time > 1] <- 1
-        
-        
+
+
       #}
-    
-      
+
+
 
   }
-  
-  
+
+
   return(output)
 }
 
 
 set.seed(123)
-pts <- simulate_patients(n.sample = 100, max_stages = 50, tau = 1000,
+pts <- simulate_patients(n.sample = 10, max_stages = 50, tau = 100,
                   ## initially all patients are at risk
                   at.risk = 1,
                   ## Life so far lived at the beginning of the stage
                   cumulative.time = 0,
-                  prior.visit.length = 0, 
+                  prior.visit.length = 0,
                   p = 1, ## "covariate" value for later state generation,
                   ## 1 for failure rate: smaller values mean larger visit times so we want this for "longer survival"
-                  
+
                   #### Failure rate parameters get really small fairly quickly
                   ## larger magnitude values of p1 will cause the rate to shift too dramatically (holding all others constant)
                   ## if we want "normal" visit lengths which can be large values, the magnitude of h (prior visit length) must be small to lessen perturbation
                   ## changing the intercept of failure.time to be larger makes the most difference in increasing overall number of ppl making it to later stages
-                  
+
                   ## -4.5, b1 = -1, z1 = -0.07, p1 = -0.05, g1 = 0.7, h1 = -0.2, r1 = -0.05
-                  
+
                   a1 = -6, b1 = -0.3, z1 = -0.025, p1 = 0.02, g1 = -0.2, h1 = 0.008, r1 = 0.01,
                   ## 2 for time to next visit
-                  
+
                   ## -1.5, b2 = -1, z2 = -0.008, p2 = -0.01, g2 = -0.7, h2 = -0.2, r2 = -0.005
                   ## it looks like this rate has the most variation, but that's because the rate is the LARGEST and most visible on the plot
                   ## I wonder if due to this, there should be less variation?
-                  
-                  a2 = -3, b2 = -0.3, z2 = -0.015, p2 = 0.025, g2 = -0.2, h2 = 0.008, r2 = 0.01, 
+
+                  a2 = -3, b2 = -0.3, z2 = -0.015, p2 = 0.025, g2 = -0.2, h2 = 0.008, r2 = 0.01,
                   a3 = -8, b3 = -0.3, z3 = -0.04, p3 = 0.02, g3 = -0.2, h3 = 0.008, r3 = 0.01,
                   rho = 0.75,
-                  # action-specific effects 
+                  # action-specific effects
                   D0 = 0,
                   D1 = 1,
-                  g = 0.25) 
+                  g = 0.25)
 
 ## NOTE: works for -4.5, a2 = -3.9
 
-pts[,, 6]
+pts[,, 1:5]
 
 
 ######### generating visualizations
